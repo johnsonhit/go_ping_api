bae47dcc69ecb02210a5f2f70d8df26ad1a2d30d
zu1k <lvguangfa@gmail.com>
2019-07-28 18:42:40 +0800

    add allow cors

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module pingapi
 go 1.12
 
 require (
+	github.com/gin-contrib/cors v1.3.0
 	github.com/gin-gonic/gin v1.4.0
 	github.com/sparrc/go-ping v0.0.0-20190613174326-4e5b6552494c
 )
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/sparrc/go-ping"
 	"net"
@@ -9,15 +10,14 @@ import (
 	"time"
 )
 
-
 func main() {
 	gin.SetMode("release")
 	r := gin.Default()
+	r.Use(cors.Default())
 	r.GET("/", rhelp)
 	r.GET("/ping", rping)
 	r.Run(":61024")
 }
-
 
 func rhelp(c *gin.Context) {
 	c.String(http.StatusOK, "example: /ping?host=baidu.com&port=80")
@@ -25,27 +25,25 @@ func rhelp(c *gin.Context) {
 
 func rping(c *gin.Context) {
 	ahost := c.DefaultQuery("host", "127.0.0.1")
-	aport,err := strconv.Atoi(c.DefaultQuery("port",  "80"))
+	aport, err := strconv.Atoi(c.DefaultQuery("port", "80"))
 	if err != nil {
 		aport = 80
 	}
-	aips,_ := net.LookupIP(ahost)
-	icmpt,isuccess := icmpPing(ahost)
-	tcpt,tsuccess := tcpPing(ahost, aport)
+	aips, _ := net.LookupIP(ahost)
+	icmpt, isuccess := icmpPing(ahost)
+	tcpt, tsuccess := tcpPing(ahost, aport)
 	c.JSON(http.StatusOK, gin.H{
 		"host": ahost,
 		"port": aport,
 		"icmp": gin.H{
 			"success": isuccess,
-			"delay": icmpt },
-		"tcp":  gin.H{
+			"delay":   icmpt},
+		"tcp": gin.H{
 			"success": tsuccess,
-			"delay": tcpt },
+			"delay":   tcpt},
 		"ip": aips,
 	})
 }
-
-
 
 func icmpPing(icmpaddr string) (float32, bool) {
 	pinger, err := ping.NewPinger(icmpaddr)
@@ -57,9 +55,8 @@ func icmpPing(icmpaddr string) (float32, bool) {
 	pinger.Count = 1
 	pinger.Run()
 	stats := pinger.Statistics()
-	return float32(float32(stats.MinRtt)/float32(time.Millisecond)), true
+	return float32(float32(stats.MinRtt) / float32(time.Millisecond)), true
 }
-
 
 func tcpPing(tcpingaddr string, tcpport int) (float32, bool) {
 	startTime := time.Now()
@@ -69,6 +66,6 @@ func tcpPing(tcpingaddr string, tcpport int) (float32, bool) {
 		return -1, false
 	} else {
 		defer conn.Close()
-		return float32(float32(endTime.Sub(startTime))/float32(time.Millisecond)), true
+		return float32(float32(endTime.Sub(startTime)) / float32(time.Millisecond)), true
 	}
 }
